ae8dd89dac8311240a763d75ee636a3300a661eb
Aubrey Portwood <code@aubreypwd.com>
2026-02-04 12:26:30 -0700

    Get different businesses on the pricing page.

--- assets/js/tweaks/textrotator.js
+++ assets/js/tweaks/textrotator.js
@@ -1,7 +1,9 @@
 $( document ).ready( function() {
-	$( ".rotate" ).textrotator( {
-		animation: "flipUp", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
-		separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
-		speed: 6000, // How many milliseconds until the next word show.
+	$( ".rotate" ).each( function() {
+		$( this ).textrotator( {
+			animation: "flipUp", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+			separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+			speed: 3000 + Math.random() * 3000, // How many milliseconds until the next word show.
+		} );
 	} );
 } );
